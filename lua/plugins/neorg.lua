966a9a31c7f232862fe113a8c1615c5857148cc4
Malcret <theotime.marcopoulos@malcret.com>
2025-08-16 22:03:31 +0200

    feat(neorg): add base setup (not enabled)

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neorg/neorg",
+  -- BUG: Doesn't support tree-sitter main branch
+  enabled = false,
+  version = "*",
+  -- WARNING: Lazy loading can cause issues
+  -- lazy = false,
+  ft = "norg",
+  config = true,
+}
